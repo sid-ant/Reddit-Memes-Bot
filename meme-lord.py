926b4a9553a84b05b4088747836394e852bd9585
Siddhant Chhabra <siddhant1042@gmail.com>
2019-05-26 22:24:34 +0530

    get meme urls

--- meme-lord.py
+++ meme-lord.py
@@ -10,25 +10,27 @@ def get_access_token():
 def get_rising(limit):
     url = "https://oauth.reddit.com/r/memes/rising" # make it configurable later
     payload = {'limit':limit}
-    bearer_token = "134025097543-kj4-JBtqY2Ln5mVEGXD1MFlEtzM" # get this from config 
+    bearer_token = "" # get this from config 
     headers = {'Authorization': "Bearer "+bearer_token,'User-Agent':'python:com.siddhantcurious.Sprintrest:v0.0.1 (by /u/ancientspell'}
     try:
         response = requests.get(url,params=payload,headers=headers)
-        print(response.url)
-        print(response.headers)
     except HTTPError as httpe:
         logging.error("http errorr ",httpe)
         exit(1)
     except RequestException as e:
         logging.error("request exception ",e)
         exit(1)
-    # print(response.text)
+
+    image_urls = []
     body = response.json()
-    print(body)
-    return None
+    for obj in body['data']['children']:
+        url = obj['data']['url']
+        image_urls.append(url)    
+    return image_urls  
     
 def main():
     limit = 5  
     images_urls = get_rising(limit=5)
+    print(images_urls)
 
 main()
